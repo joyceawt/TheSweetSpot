a9d209cba48ca6304336ccb58311d769843fa348
Joyce Au <jawai@sephora.sg>
2023-04-27 22:29:46 +0800

    add cascade and test on mysql server

--- tss_db.sql
+++ tss_db.sql
@@ -1,13 +1,37 @@
+--
+-- Disable foreign key check and autocommit
+--
+SET
+  FOREIGN_KEY_CHECKS = 0;
+
+
+SET
+  AUTOCOMMIT = 0;
+
+
+--
+-- Table structure for Customers
+--
+DROP TABLE
+  IF EXISTS Customers;
+
+
 CREATE TABLE
   Customers(
     customer_id int NOT NULL AUTO_INCREMENT,
     name varchar(50),
-    phone bigint,
-    NOT NULL,
+    phone bigint NOT NULL,
     PRIMARY KEY (customer_id)
   );
 
 
+DROP TABLE
+  IF EXISTS Orders;
+
+
+--
+-- Table structure for Orders
+--
 CREATE TABLE
   Orders(
     order_id int NOT NULL AUTO_INCREMENT,
@@ -15,10 +39,17 @@ CREATE TABLE
     order_date datetime NOT NULL,
     order_total decimal(10, 2) NOT NULL,
     PRIMARY KEY (order_id),
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE
   );
 
 
+DROP TABLE
+  IF EXISTS Drinks;
+
+
+--
+-- Table structure for Drinks
+--
 CREATE TABLE
   Drinks(
     drink_id int NOT NULL AUTO_INCREMENT,
@@ -27,6 +58,13 @@ CREATE TABLE
     drink_price decimal(10, 2) NOT NULL,
     PRIMARY KEY (drink_id)
   );
+
+
+--
+-- Table structure for OrderItems
+--
+DROP TABLE
+  IF EXISTS OrderItems;
 
 
 CREATE TABLE
@@ -40,15 +78,19 @@ CREATE TABLE
     dairy_option boolean not NULL,
     boba_option boolean not NULL,
     PRIMARY KEY (order_items_id),
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (drink_id) REFERENCES Drinks(drink_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (drink_id) REFERENCES Drinks(drink_id) ON DELETE CASCADE
   );
 
 
 --
 -- Dumping data for Customers
--- All phone numbers taken from https://www.fakepersongenerator.com/fake-phone-number 
+-- All phone numbers taken from https://www.fakepersongenerator.com/fake-phone-number
 --
+LOCK TABLES
+  Customers WRITE;
+
+
 INSERT INTO
   Customers (name, phone)
 VALUES
@@ -57,9 +99,17 @@ VALUES
   ('John Smith', 5613267407);
 
 
+UNLOCK TABLES
+;
+
+
 --
 -- Dumping data for Orders
 --
+LOCK TABLES
+  Orders WRITE;
+
+
 INSERT INTO
   Orders (customer_id, order_date, order_total)
 VALUES
@@ -68,16 +118,23 @@ VALUES
   (3, '2023-02-12 19:00:00', 21.50);
 
 
+UNLOCK TABLES
+;
+
+
 --
 -- Dumping data for OrderItems
--- drink ideas taken from https://www.singmenu.com/liho-tea-menu/
 --
+LOCK TABLES
+  Drinks WRITE;
+
+
 INSERT INTO
   Drinks (drink_name, drink_description, drink_price)
 VALUES
   (
     'Brown Sugar Lover',
-    'brown sugar, black tapioca pearls, fresh milk',
+    'Brown sugar, Black tapioca pearls, Fresh milk',
     7.00
   ),
   (
@@ -92,9 +149,18 @@ VALUES
   );
 
 
+UNLOCK TABLES
+;
+
+
 --
 -- Dumping data for OrderItems
+-- drink ideas taken from https://www.singmenu.com/liho-tea-menu/
 --
+LOCK TABLES
+  OrderItems WRITE;
+
+
 INSERT INTO
   OrderItems (
     order_id,
@@ -110,3 +176,18 @@ VALUES
   (2, 2, 1, 0.5, 0.5, 1, 1),
   (2, 3, 1, 1.0, 1.0, 1, 1),
   (3, 3, 1, 1.0, 0.5, 1, 1);
+
+
+UNLOCK TABLES
+;
+
+
+--
+-- Re-enable foreign key check and commit
+--
+SET
+  FOREIGN_KEY_CHECKS = 1;
+
+
+COMMIT
+;
