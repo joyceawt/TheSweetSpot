087829e09800f92c737e5451b8f2db077faeb895
Joyce Au <aujoy@engr.orst.edu>
2023-05-19 02:35:56 +0800

    remove unused components

--- frontend-react/src/components/DrinkList.js
+++ frontend-react/src/components/DrinkList.js
@@ -1,9 +1,7 @@
-// model after SubscriptionLiist
 import React from "react";
 
 import Drink from "../components/Drink";
 function DrinkList({ drinkLists, onEditDrink, onDeleteDrink }) {
-  // self note: {drinkArgs} are the arguments.
   return (
     <div className="d-flex flex-column justify-content-evenly custom-size">
       {drinkLists.map((drinkArgs, i) => (
@@ -19,17 +17,3 @@ function DrinkList({ drinkLists, onEditDrink, onDeleteDrink }) {
 }
 
 export default DrinkList;
-
-/*
-
-            <tbody>
-                {subsList.map((subsArg, i) => 
-                    <Subscription 
-                        key={i}
-                        subsList={subsArg} 
-                        onSubsEdit={onSubsEdit}
-                        onSubsDelete={onSubsDelete} 
-                    />)}
-            </tbody>
-
-            */
