36e27d882af0d32082be042c5349765487bd7caa
Joyce Au <aujoy@engr.orst.edu>
2023-05-22 13:16:25 +0800

    update db connector

--- backend/database/db-connector.js
+++ backend/database/db-connector.js
@@ -1,14 +1,24 @@
 // Get an instance of mysql we can use in the app
-const mysql = require('mysql')
+const mysql = require("mysql");
 
 // Create a 'connection pool' using the provided credentials
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'classmysql.engr.oregonstate.edu',
-    user: 'cs340_aujoy',
-    password: '',
-    database: 'cs340_aujoy'
-})
+const pool = mysql.createConnection({
+  connectionLimit: 10,
+  host: "classmysql.engr.oregonstate.edu",
+  user: "cs340_aujoy",
+  password: "",
+  database: "cs340_aujoy",
+  multipleStatements: true,
+});
+
+// Log if unsuccessful
+pool.connect((err) => {
+  if (!err) {
+    console.log("Successfully connect to mysql");
+  } else {
+    console.log("Failed to connect to mysql");
+  }
+});
 
 // Export it for use in our applicaiton
 module.exports.pool = pool;
